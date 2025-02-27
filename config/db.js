ca283e996f4e287d87ef71e057b91b929d8b4391
rakeshkidecha <kidecharakesh2002@gmail.com>
2025-02-27 15:35:28 +0530

    Save Change

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const env = require('dotenv').config();
 
 // for offline database 
-mongoose.connect('mongodb://127.0.0.1:27017/schoolMgmt');
+// mongoose.connect('mongodb://127.0.0.1:27017/schoolMgmt');
 
 // for online Database 
-// mongoose.connect(process.env.MONGODB_CONNECT_URI);
+mongoose.connect(process.env.MONGODB_CONNECT_URI);
 
 const db = mongoose.connection;
 
